9af54ad7eaaa6a338f6e41a8a43a93d9ef629157
rwilt <rosie.wilt@gmail.com>
2020-07-07 16:17:23 -0400

    changed seed

--- lookbook/db/seeds.rb
+++ lookbook/db/seeds.rb
@@ -15,21 +15,22 @@ OutfitItem.destroy_all
 
 user = User.create(username: "dnamgyal", password_digest: "abc123")
 
-item1 = Item.create(name: "blu top", category: "top", brand: "H&M", image: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png", user_id: user.id)
-item2 = Item.create(name: "blu jeans", category: "bottom", brand: "Forever 21", image: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png", user_id: user.id)
-item3 = Item.create(name: "blu hat", category: "headwear", brand: "Aeropostale", image: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png", user_id: user.id)
-item4 = Item.create(name: "blu cardigan", category: "top", brand: "Zoomies", image: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png", user_id: user.id)
-item5 = Item.create(name: "blu stilettos", category: "footwear", brand: "H&M", image: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png", user_id: user.id)
+item1 = Item.create(name: "tee shirt", category: "top", brand: "H&M", image: "https://i.imgur.com/keuBGFP.png", user_id: user.id)
+item2 = Item.create(name: "dress pants", category: "bottom", brand: "Forever 21", image: "https://i.imgur.com/qiCfDoM.png", user_id: user.id)
+item3 = Item.create(name: "hat", category: "headwear", brand: "Aeropostale", image: "https://i.imgur.com/Y6CyC0R.png", user_id: user.id)
+item4 = Item.create(name: "swim trunks", category: "top", brand: "Zoomies", image: "https://i.imgur.com/gUlZaWz.png", user_id: user.id)
+item5 = Item.create(name: "footwear", category: "footwear", brand: "H&M", image: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png", user_id: user.id)
+item6 = Item.create(name: "grey shirt", category: "top", brand: "Supreme", image:"https://i.imgur.com/tOoJnfH.png", user_id: user.id)
+item7 = Item.create(name: "casual blazer", category: "top", brand: "Suit Supply", image:"https://i.imgur.com/VqbUqOQ.png", user_id: user.id)
 
 outfit1 = Outfit.create(name: "summerlook", user_id: user.id)
 outfit2 = Outfit.create(name: "winterlook", user_id: user.id)
 
 OutfitItem.create(outfit_id: outfit1.id, item_id: item1.id)
-OutfitItem.create(outfit_id: outfit1.id, item_id: item2.id)
+OutfitItem.create(outfit_id: outfit1.id, item_id: item4.id)
 OutfitItem.create(outfit_id: outfit1.id, item_id: item3.id)
-OutfitItem.create(outfit_id: outfit1.id, item_id: item5.id)
 
 
-OutfitItem.create(outfit_id: outfit2.id, item_id: item1.id)
 OutfitItem.create(outfit_id: outfit2.id, item_id: item2.id)
-OutfitItem.create(outfit_id: outfit2.id, item_id: item3.id)
+OutfitItem.create(outfit_id: outfit2.id, item_id: item6.id)
+OutfitItem.create(outfit_id: outfit2.id, item_id: item7.id)
